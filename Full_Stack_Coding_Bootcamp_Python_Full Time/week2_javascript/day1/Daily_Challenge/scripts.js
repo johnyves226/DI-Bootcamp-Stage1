d67f72c6e2245177cde213bd1747e9cf54b5498a
johnyves226 <jean-yves.boiuda@loktore.com>
2022-09-29 13:19:30 +0000

    day 2 js exercise

---
+++ Full_Stack_Coding_Bootcamp_Python_Full Time/week2_javascript/day1/Daily_Challenge/scripts.js
@@ -0,0 +1,33 @@
+// exercice1
+/*
+Remove Banana from the array.
+    Sort the array in alphabetical order.
+    Add “Kiwi” to the end of the array.
+    Remove “Apples” from the array. Don’t use the same method as in part 1.
+Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+At the end you should see this outcome:
+*/
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits= fruits.sort();
+console.log(fruits);
+fruits.push("Kiwi");
+console.log(fruits);
+fruits = fruits.filter(function(item){
+    return item!=="Apples";
+})
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+
+//exercise 2
+
+
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0]);
---
+++ Full_Stack_Coding_Bootcamp_Python_Full Time/week2_javascript/day2/exerciseXPGold/scripts.js
@@ -0,0 +1,12 @@
+//exercise1
+
+/*Ask the user which language they speak.
+    Convert the user’s answer to lowercase, so that all the user’s inputs will be accepted in the if statement. For example “english” or “English” or “ENGlish” ect…”
+Create a few conditions :
+    If the user speaks French : display “Bonjour”
+If the user speaks English : display “Hello”
+If the user speaks Hebrew : display “Shalom”
+If the user doesn’t speak any of these 3 languages: display ‘01110011 01101111 01110010 01110010 01111001’*/
+
+let language = prompt("which language you speak")
+
