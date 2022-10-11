36c4ed699afa1280fc3e439d0d1251b2ce921558
johnyves226 <jean-yves.boiuda@loktore.com>
2022-10-11 12:31:08 +0000

    day 5 js xp exo

---
+++ Full_Stack_Coding_Bootcamp_Python_Full Time/week2_javascript/day5/exerciseXP/scripts.js
@@ -0,0 +1,125 @@
+var number=0
+var computerNumber=0
+
+function playTheGame() {
+    if (!confirm("voulez-vous jouer a notre formidable jeux")){
+        alert("No problem, Goodbye")
+    }
+    else{
+        number = +prompt("veuillez entrer un numbre entre 0 et 10: ")
+            if (isNaN(number)){
+                alert("Sorry Not a number, Goodbye")
+            }
+            else
+            {   if(number>0 && number>10){
+                alert("Sorry it’s not a good number, Goodbye")
+                }
+                else {
+
+                        computerNumber =  (Math.ceil(Math.random()*10))
+                        compareNumbers(number,computerNumber)
+                }
+            }
+    }
+}
+
+function compareNumbers(userNumber,computerNumber){
+    if (userNumber==computerNumber){
+        alert("WINNER")
+        return true
+    }
+
+    if (userNumber>computerNumber){
+        alert("Your number is bigger then the computer’s, guess again")
+        let number = +prompt("veuillez entrer  de nouveau un numbre entre 0 et 10: ")
+        if (isNaN(number)){
+            alert("Sorry Not a number, Goodbye")
+        }
+        else {
+            if (number == computerNumber) {
+                alert("WINNER")
+                return true
+            }
+            if (number < computerNumber) {
+                alert("Your number is smaller then the computer’s, guess again")
+                number = +prompt("veuillez entrer de nouveau numbre entre 0 et 10: ")
+                if (isNaN(number)) {
+                    alert("Sorry Not a number, Goodbye")
+                }else{
+                    if (number == computerNumber) {
+                        alert("WINNER")
+                        return true
+                    }
+                    else{
+                        alert("out of chances")
+                    }
+                }
+
+            }
+            if (userNumber > computerNumber) {
+                alert("Your number is bigger then the computer’s, guess again")
+                let number = +prompt("veuillez entrer  de nouveau un numbre entre 0 et 10: ")
+                if (isNaN(number)){
+                    alert("Sorry Not a number, Goodbye")
+                }
+                else{
+                    if (number == computerNumber) {
+                        alert("WINNER")
+                        return true
+                    }
+                    else{
+                        alert("out of chances")
+                    }
+                }
+            }
+            }
+        }
+
+    if (userNumber<computerNumber){
+        alert("Your number is smaller then the computer’s, guess again")
+        number = +prompt("veuillez entrer de nouveau numbre entre 0 et 10: ")
+        if (isNaN(number)){
+            alert("Sorry Not a number, Goodbye")
+        }else{
+
+            if (number == computerNumber) {
+                alert("WINNER")
+                return true
+            }
+            if (number < computerNumber) {
+                alert("Your number is smaller then the computer’s, guess again")
+                number = +prompt("veuillez entrer de nouveau numbre entre 0 et 10: ")
+                if (isNaN(number)) {
+                    alert("Sorry Not a number, Goodbye")
+                }else{
+                    if (number == computerNumber) {
+                        alert("WINNER")
+                        return true
+                    }
+                    else{
+                        alert("out of chances")
+                    }
+                }
+
+            }
+            if (userNumber > computerNumber) {
+                alert("Your number is bigger then the computer’s, guess again")
+                let number = +prompt("veuillez entrer  de nouveau un numbre entre 0 et 10: ")
+                if (isNaN(number)){
+                    alert("Sorry Not a number, Goodbye")
+                }
+                else{
+                    if (number == computerNumber) {
+                        alert("WINNER")
+                        return true
+                    }
+                    else{
+                        alert("out of chances")
+                    }
+                }
+            }
+
+        }
+    }
+
+}
