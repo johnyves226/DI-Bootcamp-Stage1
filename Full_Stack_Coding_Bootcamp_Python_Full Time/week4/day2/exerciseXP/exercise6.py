5994f6533706e4937c78a243a850bb7ed604ecfb
johnyves226 <jean-yves.boiuda@loktore.com>
2022-11-23 10:36:58 +0000

    w4 day 2 js xp exo and bonus

---
+++ Full_Stack_Coding_Bootcamp_Python_Full Time/week4/day2/exerciseXP/exercise6.py
@@ -0,0 +1,10 @@
+# 🌟 Exercice 6 : Boucle While
+# Des Instructions
+# Écrivez une boucle while qui demandera continuellement à l'utilisateur son nom, à moins que l'entrée ne soit égale à votre nom.
+
+print("--------Début-----------")
+
+nom ="john"
+nom_recu = input("Enter votre nom : ")
+while nom_recu != nom :
+    nom_recu = input("Entrer votre nom : ")
